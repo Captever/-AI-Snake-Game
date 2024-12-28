03afee85049917d67702070f29873a598a7bb0c4
Captever-Desktop <Captever6@gmail.com>
2024-12-29 01:21:39 +0900

    Initial map implementation

--- game.py
+++ game.py
@@ -1,30 +1,44 @@
 import pygame
 import sys
 
-# Pygame 초기화
-pygame.init()
-
-# 화면 설정
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Snake")
-
-# 색상 정의
+# define color
 WHITE = (255, 255, 255)
 GRAY = (127, 127, 127)
+BLACK = (0, 0, 0)
 
-# 게임 루프
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class Game:
+    def __init__(self):
+        # initialize Pygame
+        pygame.init()
 
-    # 화면 그리기
-    screen.fill(GRAY)
-    
-    # 화면 업데이트
-    pygame.display.flip()
+        # initialize screen
+        pygame.display.set_caption("Snake")
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen.fill(BLACK)
 
-# Pygame 종료
-pygame.quit()
-sys.exit()
+        # initialize map
+        self.mapLength = self.screen.get_width() // 2
+        self.map = pygame.Surface((self.mapLength, self.mapLength))
+        mapRect = pygame.Rect(0, 0, self.mapLength, self.mapLength)
+        pygame.draw.rect(self.map, WHITE, mapRect, 3)
+
+        self.screen.blit(self.map, ((self.screen.get_width() - self.mapLength) // 2, (self.screen.get_height() - self.mapLength) // 2))
+
+        # initialize clock
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
+            pygame.display.update()
+            self.clock.tick(60)
+
+        # end of program
+        pygame.quit()
+        sys.exit()
+
+Game().run()
