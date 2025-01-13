0ac1f74af8ca08e2ca11332fe046be75179a3160
Captever-Desktop <Captever6@gmail.com>
2025-01-14 02:51:11 +0900

    Added a case to move along the closer axis between x and y

--- ai-study/Rule-Based-AI/1_simple-move/Priority-Smaller-Difference/code.py
+++ ai-study/Rule-Based-AI/1_simple-move/Priority-Smaller-Difference/code.py
@@ -1,12 +1,14 @@
 from .base_ai import BaseAI
 
+from sys import maxsize
+
 from typing import Tuple, List
 
 def get_dist(pos_a: Tuple[int, int], pos_b: Tuple[int, int]) -> int:
-    return abs(pos_b[0] - pos_a[0]) + abs(pos_b[1] - pos_a[0])
+    return abs(pos_b[0] - pos_a[0]) + abs(pos_b[1] - pos_a[1])
 
 def get_nearest_feed_pos(head: Tuple[int, int], feed_list: List[Tuple[int, int]]) -> Tuple[int, int]:
-    min_dist = int('inf')
+    min_dist = maxsize
     ret = None
 
     for feed in feed_list:
@@ -26,11 +28,16 @@ class RuleBasedAI(BaseAI):
         if not feed_list:
             return None
         
-        target = get_nearest_feed_pos(feed_list)
+        target = get_nearest_feed_pos(head, feed_list)
 
         dx, dy = target[0] - head[0], target[1] - head[1]
 
-        if abs(dx) > abs(dy):
+        if dx == 0:
+            return 'S' if dy > 0 else 'N'
+        elif dy == 0:
+            return 'E' if dx > 0 else 'W'
+
+        if abs(dx) < abs(dy):
             return 'E' if dx > 0 else 'W'
         else:
             return 'S' if dy > 0 else 'N'
