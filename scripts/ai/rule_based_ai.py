f55c780f14d716b2fa458b005b385852fc02e93b
Captever-Desktop <Captever6@gmail.com>
2025-01-14 02:50:59 +0900

    Corrected calculations in the `get_dist()` function

--- scripts/ai/rule_based_ai.py
+++ scripts/ai/rule_based_ai.py
@@ -5,7 +5,7 @@ from sys import maxsize
 from typing import Tuple, List
 
 def get_dist(pos_a: Tuple[int, int], pos_b: Tuple[int, int]) -> int:
-    return abs(pos_b[0] - pos_a[0]) + abs(pos_b[1] - pos_a[0])
+    return abs(pos_b[0] - pos_a[0]) + abs(pos_b[1] - pos_a[1])
 
 def get_nearest_feed_pos(head: Tuple[int, int], feed_list: List[Tuple[int, int]]) -> Tuple[int, int]:
     min_dist = maxsize
@@ -23,6 +23,7 @@ def get_nearest_feed_pos(head: Tuple[int, int], feed_list: List[Tuple[int, int]]
 class RuleBasedAI(BaseAI):
     def decide_direction(self):
         head = self.game.player.bodies[0]
+        neck = self.game.player.bodies[1]
 
         feed_list = list(self.game.fs.feeds.keys())
         if not feed_list:
@@ -32,7 +33,12 @@ class RuleBasedAI(BaseAI):
 
         dx, dy = target[0] - head[0], target[1] - head[1]
 
-        if abs(dx) > abs(dy):
+        if dx == 0:
+            return 'S' if dy > 0 else 'N'
+        elif dy == 0:
+            return 'E' if dx > 0 else 'W'
+
+        if abs(dx) < abs(dy):
             return 'E' if dx > 0 else 'W'
         else:
             return 'S' if dy > 0 else 'N'
