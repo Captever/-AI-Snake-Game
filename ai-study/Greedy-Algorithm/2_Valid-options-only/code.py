b2151461c571eae073451d5801a391901d43540f
Captever-Desktop <Captever6@gmail.com>
2025-01-16 23:53:39 +0900

    Added documents about "2_Valid-options-only" of `Greedy-Algorithm`

---
+++ ai-study/Greedy-Algorithm/2_Valid-options-only/code.py
@@ -0,0 +1,26 @@
+from sys import maxsize
+
+from .base_ai import BaseAI
+
+from constants import DIR_OFFSET_DICT
+
+class GreedyAI(BaseAI):
+    def decide_direction(self):
+        # explore the optimal direction from all possible directions
+        bodies = self.game.player.bodies
+        head = bodies[0]
+        best_direction = None
+        min_distance = maxsize
+
+        for direction, offset in DIR_OFFSET_DICT.items():
+            next_position = (head[0] + offset[0], head[1] + offset[1])
+            if (not self.game.is_in_bound(next_position)) or (next_position in bodies):
+                continue
+
+            for feed in self.game.fs.feeds:
+                distance = abs(next_position[0] - feed[0]) + abs(next_position[1] - feed[1])
+                if distance < min_distance:
+                    min_distance = distance
+                    best_direction = direction
+        
+        return best_direction if best_direction is not None else "surrender"
