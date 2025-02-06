20aed52a37b033bda398a77e305e733938adbffd
Captever-Desktop <Captever6@gmail.com>
2025-02-06 23:15:17 +0900

    Added the prototype of `ReplayManager`

---
+++ scripts/manager/replay_manager.py
@@ -0,0 +1,27 @@
+import json
+
+from scripts.entity.player import Player
+from scripts.entity.feed_system import FeedSystem
+
+class ReplayManager:
+    def __init__(self):
+        pass
+        
+    def show_replay(self):
+        pass
+
+    def go_to_step(self):
+        pass
+
+    def go_to_next_step(self):
+        pass
+
+    def go_to_prev_step(self):
+        pass
+
+    def save_replay(self):
+        pass
+
+    def load_replay(self):
+        pass
+
