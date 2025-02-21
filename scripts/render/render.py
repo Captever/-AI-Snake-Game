9ca3b39d9e7665dea634d3930a9b385415b12da1
Captever-Desktop <Captever6@gmail.com>
2025-02-21 10:03:27 +0900

    `Renderer` class - completed implementation of the `render_map` function

---
+++ scripts/render/render.py
@@ -0,0 +1,57 @@
+import pygame
+
+from constants import DIR_ANGLE_DICT
+
+from scripts.ui.ui_components import Outerline
+
+from typing import List, Tuple, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from scripts.entity.feed_system import Feed
+    from scripts.ui.map_structure import Map
+
+class Renderer:
+    def render_player(self, surf, bodies: List[Tuple[int, int]]):
+        pass
+
+    def render_feeds(self, surf, feeds: List["Feed"]):
+        pass
+
+    def render_direction(self, surf, pos: Tuple[int, int], direction: str):
+        pass
+
+    def render_map(self, surf: pygame.Surface, map: "Map"):
+        grid_size = map.grid_size
+        arrow_pos = map.arrow_pos
+
+        map_rect = map.rect
+        map_outerline_thickness = map.outerline_thickness
+        map_outerline_color = map.outerline_color
+
+        grid_rect = map.grid_rect
+        grid_origin = grid_rect.topleft
+        grid_outerline_thickness = 1
+        grid_outerline_color = (255, 255, 255, 255)
+
+        cell_size = map.cell_size
+        cell_outerline_thickness = 1
+        cell_outerline_color = (255, 255, 255, 128)
+
+        for y_coord in range(grid_size[1]):
+            for x_coord in range(grid_size[0]):
+                cell_origin = (grid_origin[0] + cell_size * x_coord, grid_origin[1] + cell_size * y_coord)
+                cell_rect = pygame.Rect(cell_origin, (cell_size, cell_size))
+                self.render_outerline(surf, cell_rect, cell_outerline_thickness, cell_outerline_color)
+
+        self.render_outerline(surf, grid_rect, grid_outerline_thickness, grid_outerline_color)
+
+        self.render_outerline(surf, map_rect, map_outerline_thickness, map_outerline_color)
+    
+    def render_outerline(self, surf, rect: pygame.Rect, thickness: int = 1, color = (255, 255, 255, 255)):
+        Outerline(rect, thickness, color).render(surf)
+
+    def render_instruction(self, surf):
+        pass
+
+    def render_board(self, surf):
+        pass
