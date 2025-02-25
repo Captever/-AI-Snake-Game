872bae1e6d40c4250ff5262b10e1678cf09568e5
Captever-Desktop <Captever6@gmail.com>
2025-02-25 14:56:36 +0900

    Implemented `FeedSystem` class following the DOD approach - Moved Feedsystem-related game logic from `FeedSystem` to `Game`

--- scripts/entity/feed_system.py
+++ scripts/entity/feed_system.py
@@ -1,58 +1,51 @@
 from constants import *
 
-from scripts.manager.game_manager import GameState
-
-from typing import Tuple, Dict, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from scripts.game.base_game import BaseGame
+from typing import Tuple, Dict, List
 
 class FeedSystem:
-    def __init__(self, game: "BaseGame", feed_amount: int):
+    def __init__(self):
         """
-        Initialize FeedSystem Class
-
-        Args:
-            game (BaseGame): Current Game object
-            feed_amount (int): Amount of feeds
+        Create FeedSystem Class
         """
-        self.feeds: Dict[Tuple[int, int], Feed] = {}
-        self.game = game
-        self.feed_amount: int = feed_amount
+        self._feeds: Dict[Tuple[int, int], Feed] = {}
     
     def is_feed_empty(self, feed_type: str = 'normal'):
-        return not any(feed.type == feed_type for feed in self.feeds.values())
+        return not any(feed._type == feed_type for feed in self._feeds.values())
     
     def is_feed_exist(self, coord: Tuple[int, int]) -> bool:
         """
         Check if the given coordinates are currently in feeds list
         """
-        return coord in self.feeds
+        return coord in self._feeds
+    
+    def get_feeds(self) -> List["Feed"]:
+        return list(self._feeds.keys())
     
     def get_feed(self, coord: Tuple[int, int]) -> 'Feed':
-        return self.feeds[coord]
+        return self._feeds[coord]
 
     def add_feed(self, coord: Tuple[int, int], feed_type: str = 'normal'):
-        self.feeds[coord] = Feed(coord=coord, feed_type=feed_type)
-        self.game.cell_manager.mark_cell_used(coord)
-    
-    def add_feed_random_coord(self, k: int, feed_type: str = 'normal'):
-        if k < 1:
-            return
+        if coord in self._feeds.keys():
+            raise ValueError("Feed already exists at the inserted coordinates")
 
-        random_cell_coords = self.game.cell_manager.get_random_available_cells(k)
+        self._feeds[coord] = Feed(coord=coord, type=feed_type)
 
-        if not len(random_cell_coords):
-            self.game.set_state(GameState.CLEAR)
-            return
+    def remove_feed(self, coord: Tuple[int, int]):
+        if coord not in self._feeds.keys():
+            raise ValueError("No feed exists at the inserted coordinates")
 
-        for rand_coord in random_cell_coords:
-            self.add_feed(rand_coord, feed_type)
+        del self._feeds[coord]
 
 class Feed:
-    def __init__(self, coord: Tuple[int, int], feed_type: str):
-        self.coord = coord
-        self.type = feed_type
+    def __init__(self, coord: Tuple[int, int], type: str):
+        self._coord = coord
+        self._type = type
+
+    def get_coord(self) -> Tuple[int, int]:
+        return self._coord
+    
+    def get_type(self) -> str:
+        return self._type
 
     def to_list(self):
-        return [list(self.coord), self.type]
+        return [list(self._coord), self._type]
